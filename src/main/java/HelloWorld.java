78b0d409af8a548d6265ca6896290b7f6ea42a23
Matthew Casperson <matthewcasperson@gmail.com>
2016-01-23 14:11:21 +1000

    Changed to a hello world swing app

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,32 +1,14 @@
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
-public class HelloWorld extends Application {
+public class HelloWorld {
     public static void main(String[] args) {
-        launch(args);
-    }
+        JFrame frame = new JFrame("HelloWorldSwing");
+        final JLabel label = new JLabel("Hello World");
+        frame.getContentPane().add(label);
 
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
